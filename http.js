93479c617d94ac5e5311c9919694ed2bb553177a
OPENTEXT\mminda <mminda@opentext.com>
2022-08-18 18:39:08 +0530

    Added sample ejs and changed test script in package.json

--- http.js
+++ http.js
@@ -1,6 +1,7 @@
 var http= require("http");
 var server = http.createServer(function(req,res){
     console.log("Method:",req.method);
+	res.writeHead(200,{'Content-Type':'text/pain','test':'mehak'}); //see response headers
     res.end("Hello World!");
 });
  server.listen(3333,function(){
